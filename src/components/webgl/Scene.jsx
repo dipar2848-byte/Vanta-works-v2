5979df017481a9d38a4b32e87318b4ad5c0446df
dipar2848-byte <dipar2848@gmail.com>
2026-05-29 22:45:10 +0530

    Update Scene.jsx

--- src/components/webgl/Scene.jsx
+++ src/components/webgl/Scene.jsx
@@ -1,44 +1,35 @@
 import { Canvas } from "@react-three/fiber";
-import { ScrollControls, Scroll } from "@react-three/drei";
 
 function FloatingSphere() {
   return (
-    <mesh position={[0, 0, 0]}>
-      <sphereGeometry args={[1.5, 64, 64]} />
+    <mesh>
+      <sphereGeometry args={[2, 32, 32]} />
       <meshStandardMaterial
         color="#7c3aed"
         emissive="#22d3ee"
-        emissiveIntensity={0.6}
-        metalness={0.5}
-        roughness={0.2}
+        emissiveIntensity={0.25}
+        metalness={0.4}
+        roughness={0.7}
       />
     </mesh>
   );
 }
 
-export default function Scene({ children }) {
+export default function Scene() {
   return (
     <Canvas
-      camera={{ position: [0, 0, 5], fov: 50 }}
+      camera={{ position: [0, 0, 6], fov: 55 }}
       style={{
         position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        zIndex: 0
+        inset: 0,
+        zIndex: 0,
+        pointerEvents: "none"
       }}
     >
-      <ambientLight intensity={0.4} />
-      <directionalLight position={[5, 5, 5]} intensity={1.2} />
+      <ambientLight intensity={0.6} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
 
-      <ScrollControls pages={5} damping={0.2}>
-        <Scroll>
-          <FloatingSphere />
-        </Scroll>
-
-        <Scroll html>{children}</Scroll>
-      </ScrollControls>
+      <FloatingSphere />
     </Canvas>
   );
-        }
+}
