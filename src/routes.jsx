bc570cc848f18ffe20b550f0144892d6a357cc1c
dipar2848-byte <dipar2848@gmail.com>
2026-05-29 13:06:57 +0530

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import Admin from "./pages/Admin";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />
+  },
+  {
+    path: "/admin",
+    element: <Admin />
   }
 ]);
