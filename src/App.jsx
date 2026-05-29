48b10f07eca87d2f091f9d09d35476f8995d4730
dipar2848-byte <dipar2848@gmail.com>
2026-05-29 12:54:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,16 @@
-import { Helmet } from "react-helmet-async";
-
-import Hero from "./components/sections/Hero";
-import Problem from "./components/sections/Problem";
-import Solution from "./components/sections/Solution";
-import Pricing from "./components/sections/Pricing";
-import CTA from "./components/sections/CTA";
-
-import CursorGlow from "./components/CursorGlow";
-import Chatbot from "./components/Chatbot";
-
-import SmoothScroll from "./components/motion/SmoothScroll";
 
 export default function App() {
   return (
-    <SmoothScroll>
-      <Helmet>
-        <title>VantaWorks</title>
-      </Helmet>
-
-      <div className="bg-black text-white overflow-x-hidden">
-        <CursorGlow />
-
-        <Hero />
-        <Problem />
-        <Solution />
-        <Pricing />
-        <CTA />
-
-        <Chatbot />
-      </div>
-    </SmoothScroll>
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#000",
+        color: "#fff",
+        padding: "40px",
+        fontSize: "32px"
+      }}
+    >
+      APP WORKING
+    </div>
   );
 }
